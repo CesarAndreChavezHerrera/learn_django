19a303c4f4f33026c376880bfc3a6dd427301deb
cesar chavez <cesar.a.chavez.h@gmail.com>
2021-11-13 11:09:46 -0600

    configuracion de la app de datos

--- practica16/datos/urls.py
+++ practica16/datos/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('',views.datos_pagina,name='datos'),
+    path('',views.datos_,name='datos_'),
 ]
--- practica16/datos/views.py
+++ practica16/datos/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-def datos_pagina (request):
+def datos_(request):
     return render(request,"datos_.html")
     pass
