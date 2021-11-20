ba8365f800da91637843eabb1576f2f6d87f1715
cesar chavez <cesar.a.chavez.h@gmail.com>
2021-11-20 17:12:43 -0600

    conecion a la base de datos de conctactos

--- semana17/contacto/models.py
+++ semana17/contacto/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Contacto(models.Model):
+    nombre = models.CharField(max_length=200)
+    tema = models.CharField(max_length=200)
+    pregunta= models.TextField()
+    fecha = models.DateTimeField(default=timezone.now)
+
+
+    def __str__(self):
+        return self.tema
--- semana17/contacto/views.py
+++ semana17/contacto/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from contacto import models
 
 def contacto_pagina(r):
-    return render(r,"contacto.html")
-    pass
+    if r.method=="POST":
+        recibirnombre = r.POST.get("nombre", None)
+        recibirtema = r.POST.get("tema", None)
+        recibirpregunta = r.POST.get("pregunta", None)
+
+        models.Contacto.objects.create(
+            nombre = recibirnombre,
+            tema = recibirtema,
+            pregunta = recibirpregunta)
+    
+    c = models.Contacto.objects.all()
+    return render(r,"contacto.html",{"consulta":c})
