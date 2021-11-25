e5ffd1baeb945f306630722c65093f1bc299a10c
cesar chavez <cesar.a.chavez.h@gmail.com>
2021-11-25 16:41:02 -0600

    comprobacion de los datos ingresados

--- semana18/Ingreso/views.py
+++ semana18/Ingreso/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
+from Ingreso import models
 
 # Create your views here.
 def ingreso_datos(r):
+    if r.method == "POST":
+        r_nombre = r.POST.get("nombre")
+        r_tema = r.POST.get("tema")
+        r_pregunta = r.POST.get("pregunta")
+
+        if r_nombre != "":
+            if r_pregunta != "":
+                if r_tema !="":
+                    models.Ingreso_bd.objects.create(
+                        nombre = r_nombre,
+                        tema = r_tema,
+                        pregunta = r_pregunta
+            )
+            #print("falta nombre")
     return render(r,"catura.html")
     pass
