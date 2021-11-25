a43d358ae36c36c476093542dfd54f2df640f7d5
cesar chavez <cesar.a.chavez.h@gmail.com>
2021-11-25 16:24:19 -0600

    creacion de la base de datos

--- semana18/Ingreso/admin.py
+++ semana18/Ingreso/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from Ingreso.models import Ingreso_bd
 # Register your models here.
+
+admin.site.register(Ingreso_bd)
--- semana18/Ingreso/models.py
+++ semana18/Ingreso/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingreso_bd(models.Model):
+
+    nombre = models.CharField(max_length=100)
+    tema = models.CharField(max_length=100)
+    pregunta = models.TextField()
+
+    def __str__(self):
+        return self.nombre
+    pass
