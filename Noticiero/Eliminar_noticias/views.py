e54b9ad38a9354e19341baf33bd985374d7724d5
cesar chavez <cesar.a.chavez.h@gmail.com>
2021-11-26 20:30:07 -0600

    conexion con la base de datos para poder eliminar

--- Noticiero/Eliminar_noticias/views.py
+++ Noticiero/Eliminar_noticias/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from Portada import models
 # Create your views here.
 def eliminar_noticia(r):
-    return render(r,"eliminar_noticia.html")
+    if r.method == "POST":
+        id_noticia = r.POST.get("id",None)
+        borrar = models.Noticia_db.objects.get(id = id_noticia)
+        borrar.delete()
+        pass
+    noticias = models.Noticia_db.objects.all()
+    return render(r,"eliminar_noticia.html",{"noticias":noticias})
     pass
