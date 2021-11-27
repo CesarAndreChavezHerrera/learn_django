59d2394c57a6044fe8807879bded893b5e22a211
cesar chavez <cesar.a.chavez.h@gmail.com>
2021-11-26 20:16:53 -0600

    se conector la app eliminar con el proyecto

--- Noticiero/Noticiero/settings.py
+++ Noticiero/Noticiero/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Portada',
-    'Add_noticia'
+    'Add_noticia',
+    'Eliminar_noticias'
 ]
 
 MIDDLEWARE = [
--- Noticiero/Noticiero/urls.py
+++ Noticiero/Noticiero/urls.py
@@ -18,8 +18,10 @@ from django.urls import path
 from django.conf.urls import include
 from Portada import urls
 from  Add_noticia import urls
+from Eliminar_noticias import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',include("Portada.urls")),
-    path('escribir/',include('Add_noticia.urls'))
+    path('escribir/',include('Add_noticia.urls')),
+    path('eliminar/',include('Eliminar_noticias.urls'))
 ]
